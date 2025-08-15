df37e66b214bf770cc38553732fe3249653bb178
HasnainWebWizard <hasnainijazm321@gmail.com>
2025-08-15 16:49:04 +0500

    SVG line updated

--- src/app/containers/WhatMakesUsDiffCont.jsx
+++ src/app/containers/WhatMakesUsDiffCont.jsx
@@ -10,40 +10,60 @@ export default function WhatMakesUsDiffCont() {
                 <Heading name={'What makes us different'} />
             </div>
 
-            
+
             <section className="grid grid-rows-2 grid-cols-4 relative px-5">
 
+                {/* Middle line with responsive dash size */}
                 <div className="absolute w-full h-full flex justify-center items-center">
-                    <span className="absolute border-2 border-[#000000] w-[72%] border-dashed top-1/2 "></span>
+                    <svg
+                        className="absolute top-1/2"
+                        width="70%"
+                        height="6"
+                        preserveAspectRatio="none"
+                    >
+                        <line
+                            x1="0"
+                            y1="3"
+                            x2="100%"
+                            y2="3"
+                            stroke="#000"
+                            strokeWidth="3"
+                            strokeLinecap="round"
+                            className="sm:[stroke-dasharray:10_10] [stroke-dasharray:10_6]"
+                        />
+                    </svg>
                 </div>
 
 
+
+
+
                 <div className="row-start-2 flex flex-col items-center justify-start text-center px-1 relative -top-1">
-                    <span className="bg-[#A55C2F] rounded-full w-3 h-3 z-10 "></span>
+                    <span className="bg-[#A55C2F] rounded-full w-3 h-3 md:w-4 md:h-4 z-10 "></span>
                     <p className="text-[#BF8933] text-[10px] sm:text-sm md:text-sm lg:text-xl my-5">
                         We design with clarity, structure, and long-term growth in mind
                     </p>
                 </div>
 
-                <div className="row-start-1 col-start-2 flex flex-col items-center justify-end text-center px-1 relative -bottom-2">
+                <div className="row-start-1 col-start-2 flex flex-col items-center justify-end text-center px-1 relative -bottom-2 md:-bottom-3">
                     <p className="text-[#BF8933] text-[10px] sm:text-sm md:text-sm lg:text-xl my-5">
                         Every platform solves a real-world challenge
                     </p>
-                    <span className="bg-[#A55C2F] rounded-full w-3 h-3 z-10"></span>
+                    <span className="bg-[#A55C2F] rounded-full w-3 h-3 md:w-4 md:h-4 z-10"></span>
                 </div>
 
                 <div className="row-start-2 col-start-3 flex flex-col items-center justify-start text-center px-1 relative -top-1">
-                    <span className="bg-[#A55C2F] rounded-full w-3 h-3 z-10"></span>
+                    <span className="bg-[#A55C2F] rounded-full w-3 h-3 md:w-4 md:h-4 z-10"></span>
                     <p className="text-[#BF8933] text-[10px] sm:text-sm md:text-sm lg:text-xl my-5">
                         Everything aligns under one coherent,  values-led framework
                     </p>
                 </div>
 
-                <div className="row-start-1 col-start-4 flex flex-col items-center justify-end text-center px-1 relative -bottom-2">
+                <div className="row-start-1 col-start-4 flex flex-col items-center justify-end text-center px-1 relative -bottom-2 md:-bottom-3">
                     <p className="text-[#BF8933] text-[10px] sm:text-sm md:text-sm lg:text-xl my-5">
                         We enable individuals, teams, and entire organizations to thrive
                     </p>
-                    <span className="bg-[#A55C2F] rounded-full w-3 h-3 z-10"></span>
+                    <span className="bg-[#A55C2F] rounded-full w-3 h-3 md:w-4 md:h-4 z-10"></span>
                 </div>
             </section>
 
